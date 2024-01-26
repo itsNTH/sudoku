9083438beeb671af3841d1746d74697ad4f12ef2
Nathan-Edward SONGHO-MALONGA <SONGHO-MALONGA_Na@lprs.local>
2024-01-26 11:29:47 +0100

    th: Ajout des regles du sudoku

--- Sudoku.java
+++ Sudoku.java
@@ -5,7 +5,8 @@ public class Sudoku {
         
         
     }
-
+    
+    //Initialisation du tableau
     public int[][] tableau ={
         {0, 0, 0, 0, 0, 0, 0, 0, 0},
         {9, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -22,21 +23,57 @@ public class Sudoku {
       for(int i = 0; i < 9; i++){
         for (int k = 0; k < 9; k++){
             if (k % 3 == 0 && k != 0) {
-                System.out.print("| ");
-                
+                System.out.print("| "); 
             }
-            
             System.out.print(tableau[i][k]+" ");
             if (i % 3 == 2 && i != 0 && k == 8) {
                 System.out.println();
-                System.out.println("---------------------");
-                
+                System.out.println("---------------------");  
             }
         }
         System.out.println();
-    }
+        }
     
-}
-}
+    }
+    private boolean verifLigne(int ligne, int valeurs, int tableau[][]){
+        //Verification de la ligne
+        for (int i = 0; i < 9; i++){
+            if (tableau[ligne][i] == valeurs){
+                return false;
+            }
+        }
+        return true;
+    }
+    private boolean verifCol(int colone, int valeurs, int tableau[][]){
+        //verif colone 
+        for(int k = 0; k<9; k++){
+            if(tableau[k][colone] == valeurs){
+                return false;
+            }
 
+        }
+        return true;
+    }
+    private boolean verifBox(int colone, int valeurs, int tableau[][]){
+        //verif box 3x3
+        int boxCol = colone % 3;
+        int boxLigne = colone % 3;
+        for(int i = boxLigne * 3; i < boxLigne * 3 + 3; i++){
+            for(int k = boxCol * 3; k < boxCol * 3 + 3; k++){
+                if(tableau[i][k] == valeurs){
+                    return false;
+                }
+            }
+        }
+        return true;
+        
+
+
+    }
+
+    
+
+
+
+}
 
